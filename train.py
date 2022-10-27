93977bdc748a361fd60ec7b2d1d0e61ec84a71d1
JdRion <ljd4636@gmail.com>
2022-10-27 01:06:35 +0000

    save

--- train.py
+++ train.py
@@ -172,16 +172,16 @@ if __name__ == '__main__':
     # 터미널 실행 예시 : python3 run.py --batch_size=64 ...
     # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default='klue/roberta-small', type=str)
-    parser.add_argument('--batch_size', default=16, type=int)
-    parser.add_argument('--max_epoch', default=1, type=int)
+    parser.add_argument('--model_name', default='klue/roberta-large', type=str)
+    parser.add_argument('--batch_size', default=2, type=int)
+    parser.add_argument('--max_epoch', default=10, type=int)
     parser.add_argument('--shuffle', default=True)
     parser.add_argument('--learning_rate', default=1e-5, type=float)
     parser.add_argument('--train_path', default='../data/train.csv')
     parser.add_argument('--dev_path', default='../data/dev.csv')
     parser.add_argument('--test_path', default='../data/dev.csv')
     parser.add_argument('--predict_path', default='../data/test.csv')
-    args = parser.parse_args(args=[])
+    args = parser.parse_args()
 
     # dataloader와 model을 생성합니다.
     dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
@@ -191,8 +191,8 @@ if __name__ == '__main__':
     wandb_logger = WandbLogger(project="sts")
 
     # gpu가 없으면 'gpus=0'을, gpu가 여러개면 'gpus=4'처럼 사용하실 gpu의 개수를 입력해주세요
-    trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1,logger=wandb_logger,callbacks=[EarlyStopping(monitor="val_loss", mode="min")])
-
+    #trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1,logger=wandb_logger,callbacks=[EarlyStopping(monitor="val_pearson", mode="min")])
+    trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1,logger=wandb_logger)
     # Train part
     trainer.fit(model=model, datamodule=dataloader)
     trainer.test(model=model, datamodule=dataloader)
